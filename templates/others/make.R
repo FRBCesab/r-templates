5f85cab77d53cc682ce37154d2fb03e3829a6640
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-04-20 16:43:38 +0200

    feat: add make file

---
+++ templates/others/make.R
@@ -0,0 +1,29 @@
+#' {{project_name}}: A Research Compendium
+#'
+#' @description
+#' A paragraph providing a full description of the project and describing each
+#' step of the workflow.
+#'
+#' @author {{given}} {{family}} \email{{{email}}}
+#'
+#' @date {{date}}
+
+## Install dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load project addins (R Functions and packages) ----
+
+devtools::load_all(".")
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
